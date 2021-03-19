74cdf166cf980532a5cc54d3a4ab0b7fb77d06f5
Carayoon <46262346+stlover25@users.noreply.github.com>
2021-03-19 23:06:06 +0900

    Create Item_Archive.clj

---
+++ etc(testdome)/Item_Archive.clj
@@ -0,0 +1,25 @@
+CREATE TABLE item (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  quantity INT NOT NULL
+);
+
+CREATE TABLE item_archive (
+  archive_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+DELIMITER $$
+CREATE 
+TRIGGER
+ item_delete 
+AFTER DELETE
+ ON item 
+FOR EACH ROW
+BEGIN
+  INSERT INTO item_archive(name) VALUES (
+old.name
+);
+END;
+$$
+DELIMITER ;
