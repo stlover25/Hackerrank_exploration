c89edb7dcf72456bc2ecc2ced5c934c09be27e7c
Carayoon <46262346+stlover25@users.noreply.github.com>
2021-03-24 16:23:20 +0900

    Update Users_And_Roles.sql

--- etc(testdome)/Users_And_Roles.sql
+++ etc(testdome)/Users_And_Roles.sql
@@ -1,3 +1,28 @@
+/*
+The following two tables are used to define users and their respective roles:
+
+TABLE users
+  id INTEGER NOT NULL PRIMARY KEY,
+  userName VARCHAR(50) NOT NULL
+
+TABLE roles
+  id INTEGER NOT NULL PRIMARY KEY,
+  role VARCHAR(20) NOT NULL
+The users_roles table should contain the mapping between each user and their roles. Each user can have many roles, and each role can have many users.
+
+Modify the provided SQLite create table statement so that:
+
+Only users from the users table can exist within users_roles.
+Only roles from the roles table can exist within users_roles.
+A user can only have a specific role once.
+See the example case for more details.
+
+
+SOURCE : https://www.testdome.com/questions/sql/users-and-roles/35745?visibility=3&skillId=17
+
+*/
+
+
 CREATE TABLE users_roles (
   userId INTEGER NOT NULL,
   roleId INTEGER NOT NULL,
